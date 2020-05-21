3f1e64305328f5fd139daba283305ac3a97572b7
IvanaaM <ivanamarin67@gmail.com>
2020-05-21 22:29:47 +0200

    Initalization

---
+++ findACarServer/src/main/java/com/ftn/FindACar.java
@@ -0,0 +1,14 @@
+package com.ftn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindACar {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindACar.class, args);
+
+	}
+
+}
